118dd2a8c5f2e2de0fbeed8dc65bef346f851c1f
Héctor Valentín <hectorvalentinubeda@outlook.com>
2019-04-22 19:09:25 +0200

    Added a type sensitive constructor and a function to clone the object instance into a generic JS Object.

--- JsonCSharpClassGeneratorLib/CodeWriters/TypeScriptCodeWriter.cs
+++ JsonCSharpClassGeneratorLib/CodeWriters/TypeScriptCodeWriter.cs
@@ -46,8 +46,11 @@ namespace Xamasoft.JsonClassGenerator.CodeWriters
         public void WriteClass(IJsonClassGeneratorConfig config, TextWriter sw, JsonType type)
         {
             var prefix = GetNamespace(config, type.IsRoot) != null ? "    " : "";
+           
             var exported = !config.InternalVisibility || config.SecondaryNamespace != null;
-            sw.WriteLine(prefix + (exported ? "export " : string.Empty) + "interface " + type.AssignedName + " {");
+            sw.WriteLine(prefix + (exported ? "export " : string.Empty) + "class " + type.AssignedName + " {");
+
+            //Class properties
             foreach (var field in type.Fields)
             {
                 var shouldDefineNamespace = type.IsRoot && config.SecondaryNamespace != null && config.Namespace != null && (field.Type.Type == JsonTypeEnum.Object || (field.Type.InternalType != null && field.Type.InternalType.Type == JsonTypeEnum.Object));
@@ -60,12 +63,92 @@ namespace Xamasoft.JsonClassGenerator.CodeWriters
                 }
 
 
-                sw.WriteLine(prefix + "    " + field.JsonMemberName + (IsNullable(field.Type.Type) ? "?" : "") + ": " + (shouldDefineNamespace ? config.SecondaryNamespace + "." : string.Empty) + GetTypeName(field.Type, config) + ";");
+                sw.WriteLine(prefix + "    " + field.JsonMemberName + (IsNullable(field.Type.Type) ? "?" : "") + ": " +
+                    (shouldDefineNamespace ? config.SecondaryNamespace + "." : string.Empty) + GetTypeName(field.Type, config) + ";");
             }
-            sw.WriteLine(prefix + "}");
-            sw.WriteLine();
-        }
+            //Constructor
+            WriteDataConstructor(config, sw, type, prefix);
+            //ToService function
+            WriteToServiceFunction(config, sw, type, prefix);
 
+
+            //End of class
+            sw.WriteLine(prefix + "}");
+            //Return carriage for end of file
+            sw.WriteLine(Environment.NewLine);
+        }
+        /// <summary>
+        /// The constructor is based on a nullable argument (data) of any type 
+        /// wich has the same propierties and types of the class (or a subset of them) and provides a new instance with all of them mapped
+        /// </summary>
+        private void WriteDataConstructor(IJsonClassGeneratorConfig config, TextWriter sw, JsonType type, string prefix)
+        {
+            sw.WriteLine(Environment.NewLine);
+            sw.WriteLine(prefix + "constructor(data?){");
+            foreach (var field in type.Fields)
+            {
+                var shouldDefineNamespace = type.IsRoot && config.SecondaryNamespace != null && config.Namespace != null && (field.Type.Type == JsonTypeEnum.Object || (field.Type.InternalType != null && field.Type.InternalType.Type == JsonTypeEnum.Object));
+
+                //If the type is nullable the object must be null by default
+                if (IsNullable(field.Type.Type))
+                {
+                    //"this.property = data && data.property || null;"
+                    sw.WriteLine(prefix + "        this." + field.JsonMemberName + " = data && data." + field.JsonMemberName +
+                     " || null;");
+                }
+                else
+                {
+                    
+                    
+                    var defaultValue = "";
+                    switch (field.Type.Type)
+                    {
+                        case JsonTypeEnum.String:
+                            defaultValue = "''";
+                            break;
+                        case JsonTypeEnum.Float:
+                        case JsonTypeEnum.Integer:
+                        case JsonTypeEnum.Long:
+                            //For numeric types the default value will be zero
+                            defaultValue = "0";
+                            break;
+                        default:
+                            // The property type is not primitive, so it will provide an instance of the class
+                            defaultValue = " new " + (shouldDefineNamespace ? config.SecondaryNamespace + "." : string.Empty)
+                               + GetTypeName(field.Type, config) + "()";
+                            break;
+                    }
+
+
+
+                    sw.WriteLine(prefix + "        this." + field.JsonMemberName + " = data && data." + field.JsonMemberName +
+                        " || " + defaultValue + ";");
+                }
+
+            }
+            sw.WriteLine(prefix + "    }");
+        }
+        /// <summary>
+        /// This function will provide a generic copy of the current object instance
+        /// </summary>
+        private void WriteToServiceFunction(IJsonClassGeneratorConfig config, TextWriter sw, JsonType type, string prefix)
+        {
+            sw.WriteLine(Environment.NewLine);
+            sw.WriteLine(prefix + "public toService(){");
+            
+            sw.WriteLine(prefix + "        let obj : any = new Object;");
+            foreach (var field in type.Fields)
+            {
+                
+                sw.WriteLine(prefix + "        obj." + field.JsonMemberName + " = this." + field.JsonMemberName +
+                    ";");
+
+
+            }
+            
+            sw.WriteLine(prefix + "        return obj;");
+            sw.WriteLine(prefix + "    }");
+        }
         private bool IsNullable(JsonTypeEnum type)
         {
             return
